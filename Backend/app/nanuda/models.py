b74003b0c206614d4e9734c1a2e2bb18e2344eb5
Kweonyh0 <kweonyh0000@gmail.com>
2026-07-30 10:02:21 +0900

    권용현

--- Backend/app/nanuda/models.py
+++ Backend/app/nanuda/models.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from nanuda.database import Base
-# from enum import Enum
-from nanuda.shared.existing_tables import user_table
+
+
 
 from sqlalchemy import (
     DateTime,
@@ -16,12 +16,11 @@ from sqlalchemy import (
     UniqueConstraint,
     JSON,
     Float,
+    true,
     
 )
-from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
+from sqlalchemy.orm import (Mapped, mapped_column,)
 
-class Base(DeclarativeBase):
-    pass
 # care_groups===============================================
 class care_groups(Base):
     __tablename__ = "care_groups"
@@ -54,7 +53,7 @@ class care_group_members(Base):
         comment="참여일시"
     )
 
-    relationships: Mapped[str] = mapped_column(String(20), nullable=True,
+    relationships: Mapped[str | None] = mapped_column(String(20), nullable=True,
         comment="관계"
     )
 # care_group_members========================================
@@ -78,7 +77,7 @@ class care_group_letters(Base):
         comment="가족 방 식별번호"
     )
 
-    content: Mapped[Text] = mapped_column(Text, nullable=False, 
+    content: Mapped[str] = mapped_column(Text, nullable=False, 
         comment="편지 내용"
     )
 
@@ -113,7 +112,7 @@ class invite_codes(Base):
         comment="초대 코드 만료 일시"
     )
 
-    is_active: Mapped[bool] = mapped_column(Boolean, nullable=True,
+    is_active: Mapped[bool] = mapped_column(Boolean, nullable=False, default=True, server_default=true(),
         comment="초대 코드 사용가능 여부"    
     )
 # invite_cades==============================================
